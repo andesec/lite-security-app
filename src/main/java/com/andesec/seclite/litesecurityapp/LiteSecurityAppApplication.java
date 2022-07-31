23b634581d280a20c9458efb901cc4241248a178
nkarim <nkarim@timexperts.com.pk>
2022-07-31 17:12:35 +0500

    Initial commit with Spring Files

---
+++ src/main/java/com/andesec/seclite/litesecurityapp/LiteSecurityAppApplication.java
@@ -0,0 +1,13 @@
+package com.andesec.seclite.litesecurityapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteSecurityAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiteSecurityAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andesec/seclite/litesecurityapp/LiteSecurityAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andesec.seclite.litesecurityapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteSecurityAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
